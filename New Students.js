0965579a8e0b34cb80921e3a1cb0bf0f9a7d6040
TheDinoKid <59479142+TheDinoKid@users.noreply.github.com>
2021-05-19 21:05:59 -0400

    Update New Students.js

--- New Students.js
+++ New Students.js
@@ -1,19 +1,23 @@
 /** @NotOnlyCurrentDoc */
 function getNames() {
+  //Variable Defining
   var profiles = []
-  var hubSheet = SpreadsheetApp.getActive().getSheetByName("hub");
-  var times = [new Date().getTime()]
   var names = []
   var emails = []
+  var hubSheet = SpreadsheetApp.getActive().getSheetByName("hub");
+  
+  //pulls all possible slots where names and emails could be
   var data =(hubSheet.getRange('d3:e').getValues())
+  
+  //splits them into seperate lists
   for (i=0;i!=data.length;i++){
     names.push(data[i][0])
     emails.push(data[i][1])
   }
+  
+  //removes all blank space
   var lastRow = names.filter(String).length
   names.splice(lastRow)
   emails.splice(lastRow)
   profiles.push(names,emails)
-  times.push(new Date().getTime())
-  console.log (times[1]-times[0])
   }
